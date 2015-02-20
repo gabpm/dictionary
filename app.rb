fd616d712361b6f885e6dc67a7aeb7b1d4717768
Gabriel Puello <gabpuello@gmail.com>
2015-02-20 15:32:24 -0800

    in progress

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require("sinatra")
 require("sinatra/reloader")
 also_reload("lib/**/*.rb")
 require("./lib/word")
+require("./lib/definition")
 
 
 get("/") do
@@ -15,3 +16,28 @@ post("/words") do
   @terms = Word.all()
   erb(:index)
 end
+
+post("/words") do
+  name = params.fetch("name")
+  Word.new({:name => name}).save()
+  @terms = Word.all()
+  erb(:index)
+end
+
+# post("/definitions") do
+#   name = params.fetch("name")
+#   Definitions.new({:name => name}).save()
+#   @definitions = Definitions.all()
+#   erb(:definition)
+#
+# end
+#
+# get('/words/:id') do
+#   @word = Word.find(params.fetch('id').to_i())
+#   erb(:words)
+# end
+#
+# get('/definitions/:id') do
+#   @def = Definition.find(params.fetch('id').to_i())
+#   erb(:definitions)
+# end
--- lib/word.rb
+++ lib/word.rb
@@ -1,35 +1,35 @@
-# class Word
-#   attr_reader(:name)
-#
-#   @@words = []
-#
-#  define_method(:initialize) do |attributes|
-#    @name = attributes.fetch(:name)
-#    @id = @@words.length().+(1)
-#  end
-#
-#  define_singleton_method(:all) do
-#    @@words
-#  end
-#  define_method(:save) do
-#    @@words.push(self)
-#  end
-#
-#  define_singleton_method(:clear) do
-#    @@words = []
-#  end
-#
-#  define_singleton_method(:find) do |id|
-#    found_word = nil
-#    @@words.each() do |word|
-#      if word.id == id
-#        found_word = word
-#      end
-#    end
-#    found_word
-# end
-#
-#  define_method(:id) do
-#    @id
-# end
-# end
+class Word
+  attr_reader(:name)
+
+  @@words = []
+
+ define_method(:initialize) do |attributes|
+   @name = attributes.fetch(:name)
+   @id = @@words.length().+(1)
+ end
+
+ define_singleton_method(:all) do
+   @@words
+ end
+ define_method(:save) do
+   @@words.push(self)
+ end
+
+ define_singleton_method(:clear) do
+   @@words = []
+ end
+
+ define_singleton_method(:find) do |id|
+   found_word = nil
+   @@words.each() do |word|
+     if word.id == id
+       found_word = word
+     end
+   end
+   found_word
+end
+
+ define_method(:id) do
+   @id
+end
+end
