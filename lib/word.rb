2a9f485bb5384d69369b734a57b3d2bb507fb87b
Gabriel Puello <gabpuello@gmail.com>
2015-02-20 09:53:59 -0800

    add save and clear methods

--- lib/word.rb
+++ lib/word.rb
@@ -7,4 +7,15 @@ class Word
    @name = attributes.fetch(:name)
    @id = @@words.length().+(1)
  end
+
+ define_singleton_method(:all) do
+   @@words
+ end
+ define_method(:save) do
+   @@words.push(self)
+ end
+
+ define_singleton_method(:clear) do
+   @@words = []
+ end
 end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -9,4 +9,21 @@ describe(Word) do
      expect(test_word.name()).to(eq("repository"))
     end
   end
+
+  describe("#save") do
+    it("saves a new word") do
+      test_word = Word.new({:name => "repository"})
+      test_word.save
+      expect(Word.all()).to(eq([test_word]))
+    end
+  end
+
+  describe(".clear") do
+    it("empties all words") do
+      test_word = Word.new({:name => "repository"})
+      test_word.save
+      Word.clear
+      expect(Word.all()).to(eq([]))
+    end
+  end
 end
